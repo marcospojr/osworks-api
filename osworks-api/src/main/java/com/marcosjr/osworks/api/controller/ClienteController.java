42facb137ce101d3b3cfdcb61560a16427434146
marcosjr <marcosjr@marcosjr>
2020-04-26 14:14:12 -0300

    Atualização ordem de serviços

--- osworks-api/src/main/java/com/marcosjr/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/marcosjr/osworks/api/controller/ClienteController.java
@@ -3,9 +3,12 @@ package com.marcosjr.osworks.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.marcosjr.osworks.domain.model.Cliente;
 import com.marcosjr.osworks.domain.repository.ClienteRepository;
+import com.marcosjr.osworks.domain.service.CadastroClienteService;
 
 @RestController
 @RequestMapping("/clientes")
@@ -24,6 +28,9 @@ public class ClienteController {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private CadastroClienteService cadastroCliente;
 	
 	@GetMapping
 	public List<Cliente> listar() {
@@ -43,21 +50,33 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
-		return clienteRepository.save(cliente);
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+		return cadastroCliente.salvar(cliente);
 	}
 	
 	
 	@PutMapping("/{clienteId}")
-	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId,
 			@RequestBody Cliente cliente) {
 		if(!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
 		}
 		
 		cliente.setId(clienteId);
-		cliente = clienteRepository.save(cliente);
+		cliente = cadastroCliente.salvar(cliente);
 		
 		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cadastroCliente.excluir(clienteId);
+		
+		return ResponseEntity.noContent().build();
+		
 	}
 }
--- osworks-api/src/main/java/com/marcosjr/osworks/domain/repository/ClienteRepository.java
+++ osworks-api/src/main/java/com/marcosjr/osworks/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.marcosjr.osworks.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.marcosjr.osworks.domain.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+	Cliente findByEmail(String email);
 	
 }
